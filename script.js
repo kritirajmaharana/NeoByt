06021a64fd01115d92f92271ed397bdee021dc2e
kritirajmaharana <124510753+kritirajmaharana@users.noreply.github.com>
2024-02-12 10:22:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+var TrandingSlider = new Swiper('.slider', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    loop: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    },
+    pagination: {
+      el: '.pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+
+    const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+    if (isDarkMode) {
+        body.classList.add('dark-mode');
+    }
+
+    // Toggle dark mode on button click
+    darkModeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark-mode');
+        const isDarkModeEnabled = body.classList.contains('dark-mode');
+
+        // Store user's preference in localStorage
+        localStorage.setItem('darkMode', isDarkModeEnabled ? 'enabled' : 'disabled');
+    });
+});
